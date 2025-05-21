002f6899c3cee3222746df5f39bd220d42f06a84
juandavidgil <juandavidgil0911@gmail.com>
2025-05-21 14:30:44 -0600

    pantalla-botones

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import inicio from './inicio';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => inicio);
--- inicio.tsx
+++ inicio.tsx
@@ -1,16 +1,57 @@
-import { View, StyleSheet } from "react-native"
+import { View, StyleSheet, TouchableOpacity, Text } from "react-native"
 
-const InicioSesion = React.FC = () =>{
-    return(
-        <View style={styles.container}>
-            
-        </View>
-    )
-}
 const styles = StyleSheet.create({
  container: {
+    width:'100%',
+    backgroundColor:'#C0D7BB',
+    height:'100%',
+    display: 'flex',
+    flexDirection:'column',
+    alignItems:'center',
+    justifyContent:'center'
+ },
+ textoBienvenida:{
+    position:'absolute',
+    marginBottom:'40%',
+    fontSize:18,
+    fontFamily: 'Serif'
+ },
+ botonesInicio:{
+    display:'flex',
+    
+    backgroundColor: '#BEDCFE',
+    alignItems:'center',
+    width:'50%',
+    padding:10,
+    margin:'2%',
+    borderWidth: 2,
+    borderRadius: 50
+ },
+ containerBotones:{
+    position:'absolute',
+    width:'100%',
+    alignItems:'center',
+    marginTop:'90%'
 
  }
 })
+function IniciarSesion(): React.JSX.Element{
+    return(
+        <View style={styles.container}>
+            <Text style={styles.textoBienvenida}>
+                BIENVENIDO, ORGANIZA TUS PENDIENTES
+            </Text>
+           <View style={styles.containerBotones}>
+            <TouchableOpacity style={styles.botonesInicio}>
+                <Text>Iniciar Sesion</Text>
+            </TouchableOpacity>
+             <TouchableOpacity style={styles.botonesInicio}>
+                <Text>Registrarse</Text>
+            </TouchableOpacity>
+           </View>
 
-export default InicioSesion
+        </View>
+    )
+}
+
+export default IniciarSesion
